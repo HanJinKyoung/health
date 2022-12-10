6733d5dcfcc72598cf7516e02d0ef43a81673a90
tjoeun <tjoeun@DESKTOP-1OCG3MF>
2022-12-10 15:19:43 +0900

    commit

--- health/src/main/java/com/health/repository/HealthProVORepository.java
+++
@@ -1,9 +0,0 @@
-package com.health.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.health.entity.HealthProVO;
-
-public interface HealthProVORepository extends JpaRepository<HealthProVO, Integer> {
-
-}
--- health/src/main/java/com/health/repository/MemberVORepository.java
+++
@@ -1,9 +0,0 @@
-package com.health.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.health.entity.MemberVO;
-
-public interface MemberVORepository extends JpaRepository<MemberVO, String> {
-
-}
